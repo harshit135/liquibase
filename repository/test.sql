fea80ea0ec54c5b16b66c4fb1c5b561562400c77
harshit135 <71868020+harshit135@users.noreply.github.com>
2024-01-14 23:24:32 +0530

    Changed Table Name

--- repository/test.sql
+++ repository/test.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset harshit.1:1 
-CREATE TABLE databa_1 (
+-- changeset harshit.2:2 
+CREATE TABLE databa_2 (
    id int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY
 );
